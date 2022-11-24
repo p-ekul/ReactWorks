355053c2a2b9f38bba48ca4edc4e6131bc31333b
pekul <luke1682@gmail.com>
2022-11-24 15:33:45 +0900

    login page

--- src/App.js
+++ src/App.js
@@ -1,77 +1,111 @@
-//client/src/components/views/LoginPage/LoginPage.js
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { FileDownload } from "@mui/icons-material";
 
-
-import React, { useState } from 'react'
-import Axios from 'axios'
-import {useDispatch} from 'react-redux';
-import {loginUser} from '../../../_actions/user_action'
-
-function LoginPage(props) {     //파라미터로 props 넣어줘야함! 로그인 완료된 후 처음 화면으로 돌아가게 하기 위함 
-
-    const dispatch = useDispatch();
-
-    const [Email, setEmail] = React.useState(" ")
-    const [Password, setPassword] = React.useState(" ")
-
-    const onEmailHandler = (event) => {
-        setEmail(event.currentTarget.value)
-    }
-    
-    const onPasswordHandler = (event) => {
-        setPassword(event.currentTarget.value)
-    }
-
-    const onSubmitHandler = (event) => {
-        event.preventDefault(); //리프레시 방지-> 방지해야 이 아래 라인의 코드들 실행 가능 
-
-        // console.log('Email', Email);
-        // console.log('Password', Password);
-
-
-        let body={
-            email: Email,
-            password: Password
-        }
-
-        //디스패치로 액션 취하기
-        dispatch(loginUser(body))
-        .then(response => {
-            if(response.payload.loginSuccess) {
-                props.history.push('/')             //리액트에서 페이지 이동하기 위해서는 props.history.push() 이용.
-               										// 로그인 완료된 후 처음 화면(루트 페이지-landingpage로)으로 돌악가게 하기 
-            } else{
-                alert(' Error')
-            }
-        })
-
-
-        
-    }
-
-    return (
-        <div style={{
-            display: 'flex', justifyContent: 'center', alignItems: 'center',
-            width: '100%', height: '100vh'
-        }}>
-            
-            
-            <form style={{display: 'flex', flexDirection: 'column'}}
-                onSubmit={onSubmitHandler}
-            >
-
-                <label>Email</label>
-                <input type="email" value={Email} onChange={onEmailHandler}/>   
-                <label>Password</label>
-                <input type="password" value={Password} onChange={onPasswordHandler}/>
-
-                <br />
-                <button>
-                    Login
-                </button>
-            </form>
-
-        </div>
-    )
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default LoginPage
+const theme = createTheme();
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
